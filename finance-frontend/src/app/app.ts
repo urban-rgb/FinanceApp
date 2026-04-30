47dd6396be2335edb39cdcbc6ae6e4e7ecfef37b
urban-rgb <l.urban.work@gmail.com>
2026-04-30 23:10:59 +0200

    feat: implement core application shell

--- finance-frontend/src/app/app.ts
+++ finance-frontend/src/app/app.ts
@@ -1,12 +1,62 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+// This file is like a Controller in ASP.NET
+
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FinanceService } from './services/finance.service';
+import { UserResponseDto, TransactionResponseDto } from './models/finance.models';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('finance-frontend');
+
+export class AppComponent implements OnInit {
+  userId = 'a1b2c3d4-e5f6-4a5b-8c9d-0e1f2a3b4c5d'; // type is automaticly string
+  user = signal<UserResponseDto | null>(null); // like a variable? in C#
+  transactions = signal<TransactionResponseDto[]>([]);
+
+  // Dependency Injection - Angular searching for FinanceService in DI Container and
+  // put it in "this.financeService" field
+  constructor(private financeService: FinanceService) {}
+
+  // Similar to Start() in Unity
+  ngOnInit(): void {
+    this.loadUserData();
+    this.loadTransactions();
+  }
+
+  // Subscribing inside loadUserData() is for:
+  // At first code creating Observable object, that waiting for subscribing
+  // then we are .subscribing and activating Observable object
+  // and then Angular creating Query to API
+  // it's like C# await, but thread just keep doing its stuff, so site can display old information for a while
+  loadUserData(): void {
+    this.financeService.getUser(this.userId).subscribe(data => {
+      console.log('User data received:', data);
+      this.user.set(data);
+    });
+  }
+
+  // .subscirbe() for Observable method is like a trigger.
+  // Without .subscribe method won't work, because "Why do action, if you don't care abt the result?"
+  loadTransactions(): void {
+    this.financeService.getUserTransactions(this.userId).subscribe(data => {
+      this.transactions.set(data);
+    });
+  }
+
+  // Subscribing inside deleting is for:
+  // getting information from API 'bout new information and refreshing numbers on frontend
+  onDelete(id: string): void {
+    if (confirm('Are you sure?')) {
+      this.financeService.deleteTransaction(id).subscribe(() => {
+        this.loadTransactions();
+        this.loadUserData();
+      });
+    }
+  }
 }
